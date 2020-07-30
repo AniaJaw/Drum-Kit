ab01e521a7406e62e01bb1f61a303a4bf84583da
Ania <ajaworska987@gmail.com>
2020-07-31 00:08:02 +0100

    Clean up code

--- index.js
+++ index.js
@@ -6,20 +6,20 @@
 // });
 
 
-// Detectiong Button Press
+// Detecting Button Press
 
-for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
+for (let i = 0; i < document.querySelectorAll(".drum").length; i++) {
 
   document.querySelectorAll(".drum")[i].addEventListener("click", function click() {
 
-    var buttonInnerHTML = this.innerHTML;
+    let buttonInnerHTML = this.innerHTML;
     makeSound(buttonInnerHTML);
 
     // buttonAnimation(buttonInnerHTML);
   });
 }
 
-// Detectiong Keyboard Press
+// Detecting Keyboard Press
 
 document.addEventListener('keydown', function(event) {
 
@@ -34,37 +34,37 @@ function makeSound(key) {
 
   switch (key) {
     case "w":
-      var highTom = new Audio('sounds/high-tom.mp3');
+      const highTom = new Audio('sounds/high-tom.mp3');
       highTom.play();
       break;
 
     case "a":
-      var midTom = new Audio('sounds/mid-tom.mp3');
+      const midTom = new Audio('sounds/mid-tom.mp3');
       midTom.play();
       break;
 
     case "s":
-      var snare = new Audio('sounds/snare.mp3');
+      const snare = new Audio('sounds/snare.mp3');
       snare.play();
       break;
 
     case "d":
-      var floorTom = new Audio('sounds/floor-tom.mp3');
+      const floorTom = new Audio('sounds/floor-tom.mp3');
       floorTom.play();
       break;
 
     case "j":
-      var crash = new Audio('sounds/crash.mp3');
+      const crash = new Audio('sounds/crash.mp3');
       crash.play();
       break;
 
     case "k":
-      var bassDrum = new Audio('sounds/bass-drum.mp3');
+      const bassDrum = new Audio('sounds/bass-drum.mp3');
       bassDrum.play();
       break;
 
     case "l":
-      var hiHat = new Audio('sounds/hi-hat.mp3');
+      const hiHat = new Audio('sounds/hi-hat.mp3');
       hiHat.play();
       break;
 
